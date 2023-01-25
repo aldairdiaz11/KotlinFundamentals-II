a51cac9f145fd9fb189e9cce152728ea453170e3
aldairdiaz <irving.s21diaz@gmail.com>
2023-01-25 17:29:19 -0600

    "Add sets and maps"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -34,6 +34,29 @@ fun main() {
     val newDays = mutableListOf<String>("Mo", "Tu", "Th")
     newDays[2] = "Su"
     newDays.removeAt(0)
+
+    // Sets and maps
+    val fruitsSet = setOf("Orange", "Apple", "Grape", "Apple")
+    print("${fruitsSet.size} \n")
+
+    val newFruits = fruitsSet.toMutableList()
+    newFruits.add("Banana")
+
+    val daysOfTheWeek = mapOf(1 to "Monday", 2 to "Tuesday", 3 to "Wednesday")
+    println(daysOfTheWeek[2])
+
+    for (key in daysOfTheWeek.keys){
+        print("$key is to ${daysOfTheWeek[key]}\n")
+    }
+    val fruitsMap = mapOf("Favourite" to Fruit("Orange", 2.0), "Ok" to Fruit("Apple", 1.24))
+    println(fruitsMap.toSortedMap())
+
+    val newDaysOfTheWeek = daysOfTheWeek.toMutableMap()
+    newDaysOfTheWeek[4] = "Thursday"
+    newDaysOfTheWeek[5] = "Friday"
+
+    println(newDaysOfTheWeek.toSortedMap())
+
 }
 
 data class Fruit(val name: String, val price:Double)
